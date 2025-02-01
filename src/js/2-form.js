350e28afe1e483d469968100a56677f353a82b17
iryna <irapart1607@gmail.com>
2025-02-01 22:34:15 +0400

    finished form task

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,59 @@
+const STORAGE_KEY = "feedback-form-state";
+
+const formData = { 
+    email: "", 
+    message: "", 
+}
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+}
+
+// ------------------------------------------------------------///////////////
+
+refs.form.addEventListener('input', (e) => {
+   formData.email = e.currentTarget.elements.email.value;
+   formData.message = e.currentTarget.elements.message.value;
+   saveToLS(STORAGE_KEY, formData);
+    
+});
+
+function initPage(){
+    const dataForm = loadFromLS(STORAGE_KEY);
+    refs.form.elements.email.value = dataForm?.email || "";
+    refs.form.elements.message.value = dataForm?.message || "";
+}
+initPage();
+
+// ----------------------//////////////////////////////////////////////////////
+
+refs.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.message) {
+        alert('Fill please all fields');
+        return;
+      }
+    formData.email = e.currentTarget.elements.email.value;
+    formData.message = e.currentTarget.elements.message.value;
+    console.log(formData);
+     e.target.reset();
+     localStorage.removeItem(STORAGE_KEY);
+ });
+
+// /----------------------------------------------------------------/////////////
+
+function saveToLS(key, value){
+const jsonData = JSON.stringify(value);
+localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key){
+const body = localStorage.getItem(key);
+try {
+    const data = JSON.parse(body);
+    return data;
+} catch {
+    return body;
+}
+}
 
